2d8995058fbfa551b64d3d8360eb847d5cc6e2b7
Alex Wecker <den_alex@live.com>
2025-08-02 07:38:15 +0000

    added skill to experience

--- app/components/experience/components/experienceCard.tsx
+++ app/components/experience/components/experienceCard.tsx
@@ -1,4 +1,5 @@
-import { Card, CardContent, CardMedia, Typography } from '@mui/material';
+import { Box, LinearProgress, Typography } from '@mui/material';
+import Image from 'next/image'
 import styles from "./experienceCard.module.css";
 import React from 'react';
 import { ExperienceJSON } from '@/app/lib/database/models/experience';
@@ -9,23 +10,27 @@ interface Props {
 }
 
 const ExperienceCard = ({ experience }: Props) => {
-    const { name, image } = experience
-    return (
-        <Card className={styles.card}>
-            <CardMedia
+    const { name, image, skill } = experience
+    return <Box className={styles.card}>
+            <Image
                 className={styles.cardMedia}
-                component="img"
-                image={image}
+                src={image}
+                priority={true}
+                width={100}
+                height={100}
                 alt={name}
                 data-tech={name}
+                aria-hidden="true"
             />
-            <CardContent className={styles.cardContent}>
-                <Typography variant="h5" component="div">
-                    {name}
-                </Typography>
-            </CardContent>
-        </Card>
-    )
+            <Typography className={styles.cardTitle} variant="h5" component="span">{name} </Typography>
+            <LinearProgress
+                className={styles.cardSkill}
+                variant="determinate" value={skill}
+                sx={{
+                    height: "16px"
+                }}
+            />
+        </Box>
 }
 
 export default ExperienceCard
--- app/components/experience/experience.tsx
+++ app/components/experience/experience.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Section from '../section/section';
 import styles from "./experience.module.css";
 import ExperienceCard from './components/experienceCard';
-import { Grid, Typography } from '@mui/material';
+import { InputLabel, List, MenuItem, Select,  SelectChangeEvent , Typography } from '@mui/material';
 import { ExperienceJSON } from '@/app/lib/database/models/experience';
 
 interface Props {
@@ -10,7 +10,39 @@ interface Props {
     data: ExperienceJSON[];
 }
 
-const experience = async ({ id, data}: Props) => {
+const SortCriterium = {
+    SkillAscending: "Skill(asc)",
+    SkillDescending: "Skill(desc)",
+    NameAscending: "Name(asc)",
+    NameDescending: "Name(desc)"
+} as const
+
+type SortCriteria = typeof SortCriterium[keyof typeof SortCriterium]
+
+
+const Experience = ({ id, data}: Props) => {
+    
+    const [sortCriterium, setSortCriterium] = useState<SortCriteria>(SortCriterium.SkillDescending);
+    
+    const handleChange = (event: SelectChangeEvent) => {
+        setSortCriterium(event.target.value as SortCriteria);
+    };
+    let sortedData;
+    switch (sortCriterium) {
+        case SortCriterium.NameAscending:
+            sortedData = data.toSorted((a: ExperienceJSON, b: ExperienceJSON) => (a.name.localeCompare(b.name)));
+            break;
+        case SortCriterium.NameDescending:
+            sortedData = data.toSorted((a: ExperienceJSON, b: ExperienceJSON) => (b.name.localeCompare(a.name)));
+            break;
+        case SortCriterium.SkillAscending:
+            sortedData = data.toSorted((a: ExperienceJSON, b: ExperienceJSON) => (a.skill - b.skill));
+            break;
+        case SortCriterium.SkillDescending:
+            sortedData = data.toSorted((a: ExperienceJSON, b: ExperienceJSON) => (b.skill - a.skill));
+            break;
+    }
+
     return (
         <Section id={id} className={styles.experienceSection}>
             <Typography variant="h5" component="h3">
@@ -21,22 +53,37 @@ const experience = async ({ id, data}: Props) => {
                     Experience
                 </Typography>
             </Typography>
-            <Grid container rowSpacing={1} columnSpacing={{ xs: 1 }}>
+            <div className={styles.sortPicker}>
+                <InputLabel id="sortby-select-label">Sort by:</InputLabel>
+                <Select
+                    labelId="sortby-select-label"
+                    id="sortby-select"
+                    value={sortCriterium}
+                    onChange={handleChange}
+                >
+                    {
+                        Object.values(SortCriterium).map(criterium => 
+                            <MenuItem key={criterium} value={criterium}>{criterium}</MenuItem>
+                        )
+                    }
+                </Select>
+            </div>
+            <ul className={styles.experienceList}>
                 {
-                    data.map(exp =>
-                        <Grid item xs={6} md={4} xl={3} key={exp.name}>
+                    sortedData.map(exp =>
+                        <li key={exp.name} className={styles.experienceListItem}>
                             <ExperienceCard
                                 experience={exp}
                             />
-                        </Grid>
+                        </li>
                     )
                 }
-            </Grid>
+            </ul>
         </Section >
     )
 }
 
-export default experience
+export default Experience
 
 export const revalidate = 3600 // revalidate the data at most every hour
 export const dynamic = 'force-dynamic'
